b6857d123241760b66f0b01094626b996b023f2f
Clayton Priestley <clayton.priestley@gmail.com>
2022-12-04 01:58:04 -0600

    add more classes to nav tabs to make active text dark and bold. add images and links to data table. update formatting of badge.

--- js/github.js
+++ js/github.js
@@ -1,9 +1,17 @@
 "use strict";
 (function () {
-    document.addEventListener('click', function handleClick(event) {
-        var elem = document.querySelector("li.nav-item a.nav-link.active")
-        elem.classList.remove('active');
-        event.target.classList.add('active');
+    const anchors = document.querySelectorAll('#tab-nav li  a');
+    anchors.forEach(a => {
+        a.addEventListener('click', function handleClick(event) {
+            let target = event.currentTarget;
+            let elem = document.querySelector("li.nav-item a.nav-link.active");
+            elem.classList.remove('active');
+            elem.classList.remove('fw-bolder');
+            elem.classList.remove('text-body');
+            target.classList.add('active');
+            target.classList.add('fw-bolder');
+            target.classList.add('text-body');
+        });
     });
 }())
 
