41d075e0ec8c56c65eef19c5e76a695e9eb0a179
Clayton Priestley <clayton.priestley@gmail.com>
2023-01-04 10:52:34 -0600

    Begin Order Pizza assignment
    - create web page with bootstrap cdn and local font-awesome library
    - add header and hero sections
    - create custom css for hero section and zip code input
    - create javascript for google map search

---
+++ js/order-pizza.js
@@ -0,0 +1,9 @@
+(function() {
+    "use strict";
+
+    document.getElementById("zip-code-btn").addEventListener("click", () => {
+        // const zipCode = document.getElementById("zip-code-search").value;
+        window.open(`https://www.google.com/maps/search/%22pizza+world%22/`, "_blank");
+    });
+
+})();
