2180a1d68e22154934b456724ac4107199f8c060
Clayton Priestley <clayton.priestley@gmail.com>
2023-01-19 11:05:24 -0600

    jQuery Exercises - Traversing - add bootstrap - move css and js to separate files - refactor image swap js

---
+++ js/jquery_faq.js
@@ -0,0 +1,72 @@
+"use strict";
+
+$(document).ready(() => {
+    $("dd").addClass("invisible");
+
+    $("#toggle-answer").click(function (event) {
+        event.preventDefault();
+        $('dd').toggleClass('invisible');
+        $(this).text(
+            $(this).text() === 'Display Answers'
+                ? 'Hide Answers'
+                : 'Display Answers'
+        );
+    });
+
+    $("dt").click(function () {
+        $(this).toggleClass('highlight');
+    });
+
+    $("#toggle-last-fact").click(function (event) {
+        event.preventDefault();
+        $("ul li:last-child").toggleClass('highlight');
+        $(this).text(
+            $(this).text() !== 'Highlight Last Fact' ? 'Highlight Last Fact' : 'Unhighlight Last Fact'
+        );
+    });
+
+    $("h3").click(function () {
+        $(this).next().children().css('font-weight', 'bold');
+    });
+
+    $("li").click(function () {
+        $(this).parent().first().css('color', 'blue');
+    });
+
+    const imgSelectorFor = index => `#picture-frame > div:nth-child(${parseInt(index)}) > img`;
+    const imgAndSrc = selector => {return {element: $(selector), src: $(selector).attr('src')}};
+    // function imgAndSrc(selector) {
+    //     return {element: $(selector), src: $(selector).attr('src')};
+    // }
+
+    $(".swap").click(function(event) {
+        event.preventDefault();
+
+        const LEFT = '1';
+        const CENTER = '2';
+        const RIGHT = '3';
+
+        let positionClicked = $(this).attr("id");
+        let isLeftOrRight = positionClicked === LEFT || positionClicked === RIGHT;
+        let clickedImgSelector = imgSelectorFor(positionClicked);
+
+        if (isLeftOrRight) {
+            swap(
+                imgAndSrc(clickedImgSelector),
+                imgAndSrc(imgSelectorFor(CENTER))
+            );
+        } else {
+            swap(
+                imgAndSrc(imgSelectorFor(CENTER)),
+                imgAndSrc((Math.random() < 0.5) ? imgSelectorFor(LEFT) : imgSelectorFor(RIGHT))
+            );
+        }
+    });
+
+
+    function swap(clicked, other) {
+        $(clicked.element).attr('src', other.src);
+        $(other.element).attr('src', clicked.src);
+    }
+
+});
